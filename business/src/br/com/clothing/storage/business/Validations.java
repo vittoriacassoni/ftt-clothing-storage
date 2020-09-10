4202b81fa309f1c54e1ed863d0b50e7717ffeb9a
alveslaura <meu.lalinhaf48@gmail.com>
2020-09-10 00:37:35 -0300

    commit validações

--- business/src/br/com/clothing/storage/business/Validations.java
+++ business/src/br/com/clothing/storage/business/Validations.java
@@ -1,4 +1,56 @@
 package br.com.clothing.storage.business;
 
+import br.com.clothing.storage.comuns.enums.SizeEnum;
+import br.com.clothing.storage.comuns.enums.ColorEnum;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Validations {
+
+    private String dateFormat;
+
+    public static boolean validateDate (String dateStr) {
+
+        try {
+            Date sdf = new SimpleDateFormat("dd/MM/yyyy").parse(dateStr);
+            return true;
+
+        } catch (ParseException e) {
+            System.out.println("Digite uma data válida!");
+            return false;
+        }
+    }
+
+    public static boolean ValidateColorEnum(String color){
+
+        try
+        {
+            color.toLowerCase();
+            ColorEnum cor = Enum.valueOf(ColorEnum.class, color);
+            return true;
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Digite uma cor valida!");
+                return false;
+        }
+       }
+    public static boolean ValidateSizeEnum(String size){
+
+        try
+        {
+            size.toLowerCase();
+            SizeEnum cor = Enum.valueOf(SizeEnum.class, size);
+            return true;
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Digite uma cor valida!");
+            return false;
+        }
+    }
 }
--- console/src/br/com/clothing/storage/console/estadoConsole/EstadoConsoleCadastrar.java
+++ console/src/br/com/clothing/storage/console/estadoConsole/EstadoConsoleCadastrar.java
@@ -9,6 +9,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import br.com.clothing.storage.business.Validations;
 
 public class EstadoConsoleCadastrar extends MaquinaEstadoConsole {
 
@@ -23,7 +24,19 @@ public class EstadoConsoleCadastrar extends MaquinaEstadoConsole {
         Integer id = scanner.nextInt();
 
         System.out.println("Data de entrada: ");
-        Date enterDate = dateFormat.parse(scanner.next());
+
+
+        boolean validDate = false;
+
+        String inputDate = "";
+
+        while (!validDate)
+        {
+            inputDate = scanner.next();
+            validDate = Validations.validateDate(inputDate);
+        }
+
+        Date enterDate = new SimpleDateFormat("dd/MM/yyyy").parse(inputDate);
 
         System.out.println("Local da compra: ");
         String purchaseLocation = scanner.next();
@@ -38,12 +51,34 @@ public class EstadoConsoleCadastrar extends MaquinaEstadoConsole {
         String description = scanner.next();
 
         System.out.println("Tamanho (P, M ou G): ");
-        SizeEnum size = SizeEnum.valueOf(scanner.next().toUpperCase());
+        boolean validSize = false;
+
+        String inputSize = "";
+
+        while (!validSize)
+        {
+            inputSize = scanner.next().toUpperCase();
+            validSize = Validations.ValidateSizeEnum(inputSize);
+        }
+
+        SizeEnum size = SizeEnum.valueOf(inputSize);
+
 
         System.out.println("Cor (Rosa, Azul, Branco ou Preto): ");
-        ColorEnum color = ColorEnum.valueOf(scanner.next().toUpperCase());
+        boolean validColor = false;
 
+         String inputColor = "";
+
+         while (!validColor)
+         {
+             inputColor = scanner.next().toUpperCase();
+             validColor = Validations.ValidateColorEnum(inputColor);
+         }
+
+         ColorEnum color = ColorEnum.valueOf(inputColor);
         System.out.println("Preço da etiqueta: ");
+
+
         Double priceTag = scanner.nextDouble();
 
         System.out.println("Valor pago na compra: ");
